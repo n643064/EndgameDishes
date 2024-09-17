0f5bd609ab0d42c4aabf2624f1dad806f965eb34
n6430 <n643064@tutanota.com>
2024-09-18 00:05:49 +0200

    Initial commit

---
+++ src/main/java/n643064/endgame_dishes/Main.java
@@ -0,0 +1,12 @@
+package n643064.endgame_dishes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer
+{
+
+    @Override
+    public void onInitialize()
+    {
+    }
+}
---
+++ src/main/java/n643064/endgame_dishes/client/Client.java
@@ -0,0 +1,12 @@
+package n643064.endgame_dishes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
